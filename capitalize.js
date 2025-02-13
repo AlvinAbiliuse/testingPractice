59ea5ad445651291774bad3901023f376246115c
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-14 01:59:36 +0400

    first test passed

--- capitalize.js
+++ capitalize.js
@@ -1,14 +1,14 @@
 function capitalize(str) {
 	let n, newStr;
 	n = 0;
-	newStr = str.map((s) => {
+	newStr = str.split("").map((s) => {
 		if (n === 0) {
 			n++;
 			return s.toUpperCase();
 		}
 		return s;
 	});
-	return newStr.join();
+	return newStr.join("");
 }
 
 module.exports = capitalize;
