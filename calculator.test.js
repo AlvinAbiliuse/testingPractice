b3a159152573b2ce04b31082b3654585a0dbf76f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-14 02:29:48 +0400

    third test done

--- calculator.test.js
+++ calculator.test.js
@@ -1,8 +1,8 @@
-const [add, subtract, divide, multipy] = require("./calculator.js");
+const [add, subtract, divide, multiply] = require("./calculator.js");
 
 test("testing Calculator: ", () => {
-	expect(add(1, 5)).toBe(6);
-	expect(subtract(1, 5)).toBe(4);
+	expect(add(5, 1)).toBe(6);
+	expect(subtract(5, 1)).toBe(4);
 	expect(divide(6, 2)).toBe(3);
 	expect(multiply(6, 2)).toBe(12);
 });
