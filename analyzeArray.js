8547995028899fd3860e729092e4ddf19c9a0c64
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-14 03:15:25 +0400

    analyzeArray.js

---
+++ analyzeArray.js
@@ -0,0 +1,14 @@
+function analyzeArray(list) {
+	let arr = list;
+	arr.sort((a, b) => a - b);
+	let n = 0;
+	arr.map((l) => n + l);
+	return {
+		average: n / arr.length,
+		min: arr[0],
+		max: arr[arr.length - 1],
+		length: arr.length,
+	};
+}
+
+module.exports = analyzeArray;
